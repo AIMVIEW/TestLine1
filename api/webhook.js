888dfeef6ac99d2cf2ea20d4f46a7a5f072c0879
AIMVIEW <120380328+AIMVIEW@users.noreply.github.com>
2025-12-01 07:25:25 +0700

    test2

--- api/webhook.js
+++ api/webhook.js
@@ -1,81 +1,42 @@
-const https = require("https");
-const express = require("express");
-const app = express();
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
-const PORT = process.env.PORT || 3000;
-const TOKEN = process.env.LINE_ACCESS_TOKEN;
+    if (req.body.events?.[0]?.type === "message") {
+      const https = require("https");
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-app.get("/", (req, res) => {
-  res.sendStatus(200);
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
-});
-
-app.post("/webhook", function (req, res) {
-  res.send("HTTP POST request sent to the webhook URL!");
-  // If the user sends a message to your bot, send a reply message
-  if (req.body.events[0].type === "message") {
-    // You must stringify reply token and message data to send to the API server
-    const dataString = JSON.stringify({
-      // Define reply token
-      replyToken: req.body.events[0].replyToken,
-      // Define reply messages
-      messages: [
-        {
-          type: "text",
-          text: "Hello, user",
-        },
-        {
-          type: "text",
-          text: "May I help you?",
-        },
-      ],
-    });
-
-    // Request header. See Messaging API reference for specification
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + TOKEN,
-    };
-
-    // Options to pass into the request, as defined in the http.request method in the Node.js documentation
-    const webhookOptions = {
-      hostname: "api.line.me",
-      path: "/v2/bot/message/reply",
-      method: "POST",
-      headers: headers,
-      body: dataString,
-    };
-
-    // When an HTTP POST request of message type is sent to the /webhook endpoint,
-    // we send an HTTP POST request to https://api.line.me/v2/bot/message/reply
-    // that is defined in the webhookOptions variable.
-
-    // Define our request
-    const request = https.request(webhookOptions, (res) => {
-      res.on("data", (d) => {
-        process.stdout.write(d);
+      const dataString = JSON.stringify({
+        replyToken: req.body.events[0].replyToken,
+        messages: [
+          { type: "text", text: "Hello, user" },
+          { type: "text", text: "May I help you?" },
+        ],
       });
-    });
 
-    // Handle error
-    // request.on() is a function that is called back if an error occurs
-    // while sending a request to the API server.
-    request.on("error", (err) => {
-      console.error(err);
-    });
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + TOKEN,
+      };
 
-    // Finally send the request and the data we defined
-    request.write(dataString);
-    request.end();
+      const webhookOptions = {
+        hostname: "api.line.me",
+        path: "/v2/bot/message/reply",
+        method: "POST",
+        headers: headers,
+      };
+
+      const request = https.request(webhookOptions, (r) => {
+        r.on("data", (d) => console.log("LINE API:", d.toString()));
+      });
+
+      request.on("error", (err) => console.error("Error:", err));
+
+      request.write(dataString);
+      request.end();
+    }
+
+    res.status(200).send("OK");
+  } else {
+    res.status(200).send("LINE Webhook running");
   }
-});
+}
